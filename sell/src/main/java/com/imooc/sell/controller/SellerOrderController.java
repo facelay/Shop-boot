41b769fc1adc181a52146bb1f23d493c8a5ef1c8
yanfx <facelay@163.com>
2018-09-18 18:15:17 +0800

    update SellerOrderController File

--- sell/src/main/java/com/imooc/sell/controller/SellerOrderController.java
+++ sell/src/main/java/com/imooc/sell/controller/SellerOrderController.java
@@ -2,6 +2,8 @@ package com.imooc.sell.controller;
 
 
 import com.imooc.sell.dto.OrderDTO;
+import com.imooc.sell.enums.ResultEnum;
+import com.imooc.sell.exception.SellException;
 import com.imooc.sell.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,19 +28,90 @@ public class SellerOrderController {
     /**
      * 订单列表分页查询
      *
-     * @param page 第几页，从1开始
-     * @param size 一页有多少数据
+     * @param pageNo   第几页，从1开始
+     * @param pageSize 一页有多少数据
      * @return
      */
     @GetMapping("/list")
-    public ModelAndView list(@RequestParam(value = "page", defaultValue = "1") Integer page,
-                             @RequestParam(value = "size", defaultValue = "10") Integer size,
+    public ModelAndView list(@RequestParam(value = "pageNo", defaultValue = "1") Integer pageNo,
+                             @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
                              Map<String, Object> map) {
-        PageRequest request = new PageRequest(page - 1, size);
+        PageRequest request = new PageRequest(pageNo - 1, pageSize);
         Page<OrderDTO> orderDTOPage = orderService.findList(request);
         map.put("orderDTOPage", orderDTOPage);
-        map.put("currentPage", page);
+        map.put("currPage", pageNo);
+        map.put("pageSize", pageSize);
         return new ModelAndView("/order/list", map);
+    }
+
+    /**
+     * 取消订单
+     *
+     * @param orderId
+     * @param map
+     * @return
+     */
+    @GetMapping("/cancel")
+    public ModelAndView cancel(@RequestParam("orderId") String orderId,
+                               Map<String, Object> map) {
+        try {
+            OrderDTO orderDTO = orderService.findOne(orderId);
+            orderService.cancel(orderDTO);
+        } catch (SellException e) {
+            log.error("【卖家端取消订单】发生异常-->{}", e);
+            map.put("msg", e.getMessage());
+            map.put("url", "/sell/seller/order/list");
+            return new ModelAndView("common/error", map);
+        }
+        map.put("msg", ResultEnum.ORDER_CANCEL_SUCCESS.getMessage());
+        map.put("url", "/sell/seller/order/list");
+        return new ModelAndView("common/success", map);
+    }
+
+    /**
+     * 订单详情
+     * @param orderId
+     * @param map
+     * @return
+     */
+    @GetMapping("/detail")
+    public ModelAndView detail(@RequestParam("orderId") String orderId,
+                               Map<String, Object> map) {
+        OrderDTO orderDTO = new OrderDTO();
+        try {
+            orderDTO = orderService.findOne(orderId);
+        } catch (SellException e) {
+            log.error("【卖家端查询订单详情】发生异常-->{}", e);
+            map.put("msg", e.getMessage());
+            map.put("url", "/sell/seller/order/list");
+            return new ModelAndView("common/error", map);
+        }
+        map.put("orderDTO", orderDTO);
+        return new ModelAndView("order/detail", map);
+    }
+
+    /**
+     * 完结订单
+     * @param orderId
+     * @param map
+     * @return
+     */
+    @GetMapping("/finish")
+    public ModelAndView finish(@RequestParam("orderId") String orderId,
+                               Map<String, Object> map) {
+        OrderDTO orderDTO = new OrderDTO();
+        try {
+            orderDTO = orderService.findOne(orderId);
+            orderService.finish(orderDTO);
+        } catch (SellException e) {
+            log.error("【卖家端完结订单】发生异常-->{}", e);
+            map.put("msg", e.getMessage());
+            map.put("url", "/sell/seller/order/list");
+            return new ModelAndView("common/error", map);
+        }
+        map.put("msg", ResultEnum.ORDER_FINISH_SUCCESS.getMessage());
+        map.put("url", "/sell/seller/order/list");
+        return new ModelAndView("common/success", map);
     }
 
 }
